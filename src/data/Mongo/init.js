b0d62d824dbbff9987f6f709c232fc0bbe4ffd76
Erwan Poncin <erwan.poncin@etu.univ-grenoble-alpes.fr>
2025-05-07 13:15:27 +0200

    add pdf files for drawio

--- src/data/Mongo/init.js
+++ src/data/Mongo/init.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const path = '/data/import/generated_products.json'; // Chemin du fichier JSON
+const path = '/data/import/generated_products.json'; 
 
-// Lire le fichier JSON et l'insérer dans MongoDB
-const products = JSON.parse(fs.readFileSync(path, 'utf8')); // Utilisation de fs.readFileSync pour lire le fichier
+const products = JSON.parse(fs.readFileSync(path, 'utf8'));
 db = db.getSiblingDB('products_datas');
-db.iron_products.drop();  // Si tu veux supprimer la collection avant de la réinitialiser
-db.iron_products.insertMany(products);  // Insérer les produits dans la collection
+db.iron_products.drop(); 
+db.iron_products.insertMany(products);
